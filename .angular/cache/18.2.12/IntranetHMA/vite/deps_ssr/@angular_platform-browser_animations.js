20905e4624d5f36c5077c26cabd3ba2cc98c402b
MisaVPio <86525121+MisaVPio@users.noreply.github.com>
2024-12-10 21:37:54 -0300

    ...

---
+++ .angular/cache/18.2.12/IntranetHMA/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,24 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-STRVAGKP.js";
+import "./chunk-S3JCAGRY.js";
+import "./chunk-6EX65CZR.js";
+import "./chunk-AWWGSX4I.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-44JQ5FZC.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
